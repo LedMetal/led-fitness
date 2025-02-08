f2b13c6eea19ecd26c78c14838668ef2655d0a71
Abdul Sadiq <sadiq9707336@gmail.com>
2025-02-08 05:07:46 -0500

    create routes to ensure landing on AuthHome when not logged in

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
+import { AuthHomeComponent } from './auth/components/auth-home/auth-home.component';
+import { LoginComponent } from './auth/components/login/login.component';
+import { SignUpComponent } from './auth/components/sign-up/sign-up.component';
+import { HomeComponent } from './home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'auth',
+    component: AuthHomeComponent,
+    children: [
+      { path: 'login', component: LoginComponent },
+      { path: 'signup', component: SignUpComponent },
+    ],
+  },
+  {
+    path: 'home',
+    component: HomeComponent,
+  },
+  {
+    path: '',
+    component: HomeComponent,
+  },
+];
