1cbdd700a3958be0d12de6fd6df6d39fc41a722c
Abdul Sadiq <sadiq9707336@gmail.com>
2025-02-08 05:07:06 -0500

    create HomeComponent

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,24 @@
+import { Component, inject } from '@angular/core';
+import { Router, RouterModule } from '@angular/router';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faRightFromBracket } from '@fortawesome/free-solid-svg-icons';
+import { AuthService } from '../auth/services/auth.service';
+
+@Component({
+  selector: 'app-home',
+  imports: [RouterModule, FontAwesomeModule],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.scss',
+})
+export class HomeComponent {
+  authService = inject(AuthService);
+  router = inject(Router);
+
+  faRightFromBracket = faRightFromBracket;
+
+  logout(): void {
+    this.authService.logout().subscribe(() => {
+      this.router.navigateByUrl('/auth/login');
+    });
+  }
+}
