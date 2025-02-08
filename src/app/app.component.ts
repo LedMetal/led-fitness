ba596ebc698e285e1616a52ace199a57dee368fe
Abdul Sadiq <sadiq9707336@gmail.com>
2025-02-08 05:08:41 -0500

    subscribe to listen to sign ins and outs to update currentUser signal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,34 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, inject, OnDestroy, OnInit } from '@angular/core';
+import { Router, RouterOutlet } from '@angular/router';
+import { AuthService } from './auth/services/auth.service';
+import { User } from '@angular/fire/auth';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
-export class AppComponent {
-  title = 'LedFitness';
+export class AppComponent implements OnInit {
+  authService = inject(AuthService);
+  router = inject(Router);
+
+  ngOnInit(): void {
+    this.authService.user$.subscribe((user: User | null) => {
+      if (user) {
+        this.authService.currentUser.set({
+          email: user.email!,
+          username: user.displayName!,
+        });
+      } else {
+        this.authService.currentUser.set(null);
+      }
+    });
+
+    if (!this.authService.currentUser()) {
+      this.router.navigateByUrl('auth/login');
+    } else {
+      this.router.navigateByUrl('home');
+    }
+  }
 }
