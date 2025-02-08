59104c576d2cb7efa3f0b52f5723c4b0ae0c2085
Abdul Sadiq <sadiq9707336@gmail.com>
2025-02-08 05:03:05 -0500

    configure firebase

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,23 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
 import { routes } from './app.routes';
 
+const firebaseConfig = {
+  apiKey: 'AIzaSyDL8zNtCEMkmTAjGjV3qZi3JFyfo5w7Tuw',
+  authDomain: 'led-fitness-9edae.firebaseapp.com',
+  projectId: 'led-fitness-9edae',
+  storageBucket: 'led-fitness-9edae.firebasestorage.app',
+  messagingSenderId: '1080888119156',
+  appId: '1:1080888119156:web:63bfc9aba1943718dee621',
+};
+
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideFirebaseApp(() => initializeApp(firebaseConfig)),
+    provideAuth(() => getAuth()),
+  ],
 };
