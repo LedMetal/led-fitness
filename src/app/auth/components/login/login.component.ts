1dcd8232689a4b6d9c83e0b83033ff5b9b1e74ed
Abdul Sadiq <sadiq9707336@gmail.com>
2025-02-08 05:04:45 -0500

    create LoginComponent

---
+++ src/app/auth/components/login/login.component.ts
@@ -0,0 +1,35 @@
+import { Component, inject } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router, RouterModule } from '@angular/router';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faRightToBracket } from '@fortawesome/free-solid-svg-icons';
+import { AuthService } from '../../services/auth.service';
+
+@Component({
+  selector: 'app-login',
+  imports: [ReactiveFormsModule, FontAwesomeModule, RouterModule],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.scss',
+})
+export class LoginComponent {
+  formBuilder = inject(FormBuilder);
+  authService = inject(AuthService);
+  router = inject(Router);
+
+  faRightToBracket = faRightToBracket;
+
+  loginForm = this.formBuilder.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', Validators.required],
+  });
+
+  onSubmit(): void {
+    const { email, password } = this.loginForm.value;
+
+    if (email && password) {
+      this.authService.login(email, password).subscribe(() => {
+        this.router.navigateByUrl('');
+      });
+    }
+  }
+}
