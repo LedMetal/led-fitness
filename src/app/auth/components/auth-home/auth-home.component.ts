ebd5d6c159a421c61daed1aec7e94a0f5aa76552
Abdul Sadiq <sadiq9707336@gmail.com>
2025-02-08 22:25:31 -0500

    create section for action buttons in AuthHome

--- src/app/auth/components/auth-home/auth-home.component.ts
+++ src/app/auth/components/auth-home/auth-home.component.ts
@@ -1,14 +1,24 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, inject } from '@angular/core';
+import { Router, RouterModule, RouterOutlet } from '@angular/router';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { faDumbbell } from '@fortawesome/free-solid-svg-icons';
+import {
+  faCirclePlus,
+  faDumbbell,
+  faRightToBracket,
+} from '@fortawesome/free-solid-svg-icons';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-auth-home',
-  imports: [RouterOutlet, FontAwesomeModule],
+  imports: [RouterOutlet, FontAwesomeModule, RouterModule],
   templateUrl: './auth-home.component.html',
   styleUrl: './auth-home.component.scss',
 })
 export class AuthHomeComponent {
+  router = inject(Router);
+  authService = inject(AuthService);
+
+  faCirclePlus = faCirclePlus;
   faDumbbell = faDumbbell;
+  faRightToBracket = faRightToBracket;
 }
