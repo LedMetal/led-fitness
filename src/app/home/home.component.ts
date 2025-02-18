6d4b10a36cdfd7de5636dbf1539a9a2a7b40a79e
Abdul Sadiq <sadiq9707336@gmail.com>
2025-02-18 00:14:08 -0500

    subscribe to window:resize event to check for mobile screen

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,12 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, inject, OnDestroy, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { FirestoreService } from '../services/firestore.service';
 import { IUserData, IWorkoutData } from '../shared/models';
 import { AuthService } from '../auth/services/auth.service';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { Platform } from '@angular/cdk/platform';
+import { debounceTime, fromEvent, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-home',
@@ -12,13 +14,22 @@ import { NavbarComponent } from './components/navbar/navbar.component';
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
   firestoreService = inject(FirestoreService);
   authService = inject(AuthService);
+  platform = inject(Platform);
 
   noWorkoutData: boolean = false;
+  isMobile: boolean = false;
+  resizeSubscription!: Subscription;
 
   ngOnInit(): void {
+    this.resizeSubscription = fromEvent(window, 'resize')
+      .pipe(debounceTime(300))
+      .subscribe(() => {
+        this.checkIfMobile();
+      });
+
     this.firestoreService
       .getUserAndWorkoutsData()
       .subscribe((data: [IUserData, IWorkoutData[]]) => {
@@ -26,5 +37,18 @@ export class HomeComponent implements OnInit {
 
         this.noWorkoutData = data[1].length === 0;
       });
+  }
+
+  ngOnDestroy(): void {
+    this.resizeSubscription.unsubscribe();
+  }
+
+  /**
+   * Check if the current platform is either an IOS or Android device.
+   * This will also return true if the window width is less than 768px.
+   */
+  checkIfMobile(): void {
+    this.isMobile =
+      this.platform.IOS || this.platform.ANDROID || window.innerWidth < 768;
   }
 }
